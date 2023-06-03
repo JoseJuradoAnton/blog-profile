3dbfa1e6041a5313649fba457d3f5421ed956baf
Fr13nds202201 <pythonhack22@gmail.com>
2023-06-03 10:40:31 -0500

    trhee commit

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,6 @@ const db = new Sequelize({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT,
     logging: false,
 });
