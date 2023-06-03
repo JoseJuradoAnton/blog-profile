556a1b70cf08cfa501f8d361faace8b4af5e9d5a
Fr13nds202201 <pythonhack22@gmail.com>
2023-06-03 09:09:07 -0500

    firscommet

---
+++ src/models/user.model.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+const { db } = require('./../database/database');
+
+const User = db.define('users', {
+    id: {
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: true,
+        type: DataTypes.INTEGER,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    role: {
+        type: DataTypes.ENUM('user', 'admin'),
+        allowNull: false,
+        defaultValue: 'user'
+    },
+    profileImgUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'https://www.imgen.com'
+    },
+    status: {
+        type: DataTypes.ENUM('active', 'disabled'),
+        allowNull: false,
+        defaultValue: 'active'
+    },
+})
+
+module.exports = User;
